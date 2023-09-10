9c538d6c2b55912e531632ac53eb59928509b80e
Kirill Mikhailov <kirill.mikhailov.d@yandex.ru>
2023-09-10 18:36:14 +0300

    День 2.

---
+++ src/main/java/org/example/Ceasar.java
@@ -0,0 +1,8 @@
+package org.example;
+import lombok.Data;
+
+@Data
+public class Ceasar {
+    private final String text;
+    private final Integer count;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,32 +11,83 @@ import java.net.http.HttpResponse;
 
 public class Main {
         public static void main(String[] args) {
-            String json = "{\"name\":\"КЕС\",\"gitHubUrl\":\"https://github.com/Chernosmaga/developer-day\"," +
-                    "\"participants\":[{\"email\":\"sergeychernosmaga1997@ya.ru\",\"cohort\":\"java_25\",\"firstName\"" +
-                    ":\"Сергей\",\"lastName\":\"Черносмага\"},{\"email\":\"kirill.mikhailov.d@yandex.ru\",\"cohort\":" +
-                    "\"java_26\",\"firstName\":\"Кирилл\",\"lastName\":\"Михайлов\"},{\"email\":\"gks-08@mail.ru\"," +
-                    "\"cohort\":\"java_21\",\"firstName\":\"Евгений\",\"lastName\":\"Семененко\"}]}";
+            String respJson = get();
+            Document document = Jsoup.parse(respJson);
+            String h1 = document.select("#message").toString();
+            String code = h1.substring((h1.indexOf("encoded\":") + 11), h1.indexOf("\", \"offset")).trim();
+            String count = h1.substring((h1.indexOf(", \"offset\":") + 13), h1.indexOf("\"}</s")).trim();
+            String result = decrypt(code, Integer.parseInt(count));
+            String newJson = String.format("{\"decoded\": \"%s\"}", result);
+            System.out.println(post(newJson));
+        }
 
-            URI uri = URI.create("http://ya.praktikum.fvds.ru:8080/dev-day/register");
-            HttpRequest request = HttpRequest.newBuilder()
-                    .POST(HttpRequest.BodyPublishers.ofString(json))
-                    .uri(uri)
-                    .header("Content-Type", "application/json")
-                    .header("MAIN_ANSWER", "42")
-                    .build();
-            HttpClient client = HttpClient.newHttpClient();
-            try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                if (response.statusCode() != 200) {
-                    System.out.println(response.body());
-                    System.out.println("Something went wrong. Status code is: " + response.statusCode());
-                } else {
-                    System.out.println(response.body());
-                    System.out.println(response.statusCode());
-                }
-            } catch (IOException | InterruptedException e) {
-                System.out.println("There is a problem\n" +
-                        "Check the address and try again");
+    private static String get() {
+        URI uri = URI.create("http://ya.praktikum.fvds.ru:8080/dev-day/task/2");
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .header("AUTH_TOKEN", "a09dc786-2d73-479d-8a3f-d7ef514dc3f8")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                return null;
+            } else {
+                return response.body();
+            }
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    private static String post(String json) {
+        URI uri = URI.create("http://ya.praktikum.fvds.ru:8080/dev-day/task/2");
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .header("AUTH_TOKEN", "a09dc786-2d73-479d-8a3f-d7ef514dc3f8")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                System.out.println(response.body());
+                System.out.println("Something went wrong. Status code is: " + response.statusCode());
+                return null;
+            } else {
+                System.out.println(response.body());
+                System.out.println(response.statusCode());
+                return response.body();
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("There is a problem\n" +
+                    "Check the address and try again");
+            return null;
+        }
+    }
+
+    public static String encrypt(String message, int shift) {
+        message = message.toLowerCase();
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            char ch = message.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                char shiftedCh = (char) (((ch - 'a' + shift) % 26) + 'a');
+                encryptedMessage.append(shiftedCh);
+            } else {
+                encryptedMessage.append(ch);
             }
         }
+
+        return encryptedMessage.toString();
+    }
+
+    public static String decrypt(String encryptedMessage, int shift) {
+        return encrypt(encryptedMessage, 26 - shift);
+    }
 }
